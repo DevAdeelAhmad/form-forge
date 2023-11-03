e584f4b90184b0c479e541ea6360680ad2612257
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-11-04 00:42:34 +0500

    Next-Themes fr dark mode and shadcn compsinstalled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/providers/ThemeProvider"
 import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -17,8 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html suppressHydrationWarning lang="en">
+        <body className={inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
