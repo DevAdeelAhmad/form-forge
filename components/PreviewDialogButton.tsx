1c46d5553d97d072509b54b29de45e52ed0ac9db
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-11-10 22:53:21 +0500

    Working on Preview

--- components/PreviewDialogButton.tsx
+++ components/PreviewDialogButton.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Button } from './ui/button'
 import { MdPreview } from 'react-icons/md'
+import useDesigner from './hooks/useDesigner'
+import { Dialog } from './ui/dialog';
+import { DialogContent, DialogTrigger } from '@radix-ui/react-dialog';
 
 function PreviewDialogButton() {
+  const { elements } = useDesigner();
   return (
-    <Button variant={'outline'} className='gap-2'>
-      <MdPreview className="h-6 w-6" />
-      Preview
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={'outline'} className='gap-2'>
+          <MdPreview className="h-6 w-6" />
+          Preview
+        </Button>
+      </DialogTrigger>
+      <DialogContent className='w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0'>
+        <div className="px-4 py-2 border-b">
+          <p className='text-lg font-bold text-muted-foreground'>
+            Form Preview
+          </p>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
 
